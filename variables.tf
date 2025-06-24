6cb3d085d1fafa2a302559a05f5dca6ea082f061
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-25 02:03:29 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,18 +3,18 @@
 variable "template_url" {
   description = "URL of the CloudFormation template"
   type        = string
-  default = "https://demobucketforservicecatalog.s3.us-east-1.amazonaws.com/todaydemo/bucket-old.yaml"
+  default = "https://your-unique-sc-bucket-name.s3.us-east-1.amazonaws.com/helloworld.yaml"
 }
 
 variable "launch_role_arn" {
   description = "ARN of the IAM role for launching the product"
   type        = string
-  default = "arn:aws:iam::676206899900:role/servicecatalogaccesss"
+  default = "arn:aws:iam::650251701672:role/ServiceCatalogLaunchRole"
 }
 
 variable "user_arn" {
   description = "ARN of the IAM user for accessing the portfolio"
   type        = string
-  default = "arn:aws:iam::676206899900:user/svc"
+  default = "arn:aws:iam::650251701672:user/bmware_terraform"
 
 }
