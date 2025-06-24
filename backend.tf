99ac5e135d456a57584ff234c5f92b23a0d1f0ad
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-25 02:04:59 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "my-oidc-bucket-15328069840"
-    key            = "dragon1/terraform.tfstate"
+    key            = "dragon2/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     use_lockfile   = false
