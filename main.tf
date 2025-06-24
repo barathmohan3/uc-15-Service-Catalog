2814c01f354832f284e33e376851f2cff9e68192
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-24 22:06:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket" "artifact_bucket" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_object" "helloworld_tar" {
+resource "aws_s3_object" "helloworld_tar" {
   bucket = aws_s3_bucket.artifact_bucket.id
   key    = "helloworld.tar.gz"
   source = "${path.module}/helloworld.tar.gz"
@@ -18,13 +18,15 @@ resource "aws_iam_role" "launch_role" {
   name = "ServiceCatalogLaunchRole"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "servicecatalog.amazonaws.com"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "servicecatalog.amazonaws.com"
+        }
       }
-    }]
+    ]
   })
 }
 
@@ -39,16 +41,17 @@ resource "aws_cloudformation_stack" "sc_stack" {
   template_body = file("${path.module}/templates/sc_setup.yaml")
 
   parameters = {
-    PortfolioId = aws_servicecatalog_portfolio.s3_portfolio.id
-    ArtifactUrl = "https://${aws_s3_bucket.artifact_bucket.bucket}.s3.amazonaws.com/${aws_s3_bucket_object.helloworld_tar.key}"
-    RoleArn     = var.launch_role_arn
+    PortfolioId = aws_servicecatalog_portfolio.portfolio.id
+    ArtifactUrl = "https://${aws_s3_bucket.artifact_bucket.bucket}.s3.amazonaws.com/${aws_s3_object.helloworld_tar.key}"
+    RoleArn     = aws_iam_role.launch_role.arn
   }
 
   capabilities = ["CAPABILITY_NAMED_IAM"]
 }
 
 resource "aws_servicecatalog_portfolio_share" "org_share" {
-  portfolio_id = aws_servicecatalog_portfolio.s3_portfolio.id
+  portfolio_id = aws_servicecatalog_portfolio.portfolio.id
+
   organization_node {
     type  = "ORGANIZATION"
     value = var.org_id
