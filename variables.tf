2eed073053272e71477e49eecf70d58a3e876c0d
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-24 19:53:33 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,29 +1,5 @@
-variable "region" {
-  default = "us-east-1"
-}
-
-variable "bucket_name" {
-  default = "sc-artifact-demo-helloworld" # Must be globally unique
-}
-
-variable "helloworld_source_path" {
-  description = "Path to the HelloWorld tar.gz downloaded from GitHub"
-  type        = string
-}
-
-variable "portfolio_name" {
-  default = "HelloWorldPortfolio"
-}
-
-variable "portfolio_owner" {
-  default = "BM"
-}
-
-variable "product_name" {
-  default = "HelloWorldProduct"
-}
-
-variable "organization_id" {
-  description = "Your AWS Organization ID (e.g., o-xxxxxxxxxx)"
-  type        = string
-}
+variable "region" {}
+variable "bucket_name" {}
+variable "portfolio_name" {}
+variable "portfolio_owner" {}
+variable "organization_id" {}
