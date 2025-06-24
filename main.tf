69b02cc164c1ace66c019f092e64b33e81ec61d0
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-24 22:51:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -66,6 +66,6 @@ resource "aws_cloudformation_stack" "sc_stack" {
 # Share the Portfolio with the organization
 resource "aws_servicecatalog_portfolio_share" "org_share" {
   portfolio_id = aws_servicecatalog_portfolio.portfolio.id
-  principal_id = 650251701672  # Must be o-xxxxxxxx
+  principal_id = 211125784755  # Must be o-xxxxxxxx
   type         = "ACCOUNT"
 }
