ea280ac769616197e10d5fa7a3c66cac830ef52f
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-24 22:11:47 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,22 @@
-variable "region" {}
-variable "bucket_name" {}
-variable "portfolio_name" {}
-variable "portfolio_owner" {}
-variable "organization_id" {}
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "Unique S3 bucket name to store artifacts"
+}
+
+variable "portfolio_name" {
+  description = "Name of the Service Catalog portfolio"
+  default     = "HelloWorldPortfolio"
+}
+
+variable "portfolio_owner" {
+  description = "Owner of the portfolio"
+  default     = "YourCompany"
+}
+
+variable "org_id" {
+  description = "AWS Organization ID (e.g., o-xxxxxxx)"
+}
