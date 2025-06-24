4743997d678e813a1d041e356aabe25ea9015b6c
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-24 22:36:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,61 +13,59 @@ provider "aws" {
   region = var.region
 }
 
-# S3 Bucket to store the tar.gz artifact
+# Create S3 bucket to store CloudFormation template
 resource "aws_s3_bucket" "artifact_bucket" {
   bucket        = var.bucket_name
   force_destroy = true
 }
 
-# Upload the helloworld.tar.gz to S3
-resource "aws_s3_object" "helloworld_tar" {
+# Upload the CloudFormation template to S3
+resource "aws_s3_object" "helloworld_template" {
   bucket = aws_s3_bucket.artifact_bucket.id
-  key    = "helloworld.tar.gz"
-  source = "${path.module}/helloworld.tar.gz"
-  etag   = filemd5("${path.module}/helloworld.tar.gz")
+  key    = "helloworld.yaml"
+  source = "${path.module}/artifacts/helloworld.yaml"
+  etag   = filemd5("${path.module}/artifacts/helloworld.yaml")
 }
 
-# IAM Role used as a launch constraint
+# IAM Role used by Service Catalog to launch the product
 resource "aws_iam_role" "launch_role" {
   name = "ServiceCatalogLaunchRole"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "servicecatalog.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "servicecatalog.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
   })
 }
 
-# Portfolio
+# Create the Portfolio
 resource "aws_servicecatalog_portfolio" "portfolio" {
   name          = var.portfolio_name
   description   = "Portfolio created by Terraform"
   provider_name = var.portfolio_owner
 }
 
-# CloudFormation Stack to create the SC Product + associate + launch constraint
+# Create a CloudFormation stack to provision the SC Product and LaunchConstraint
 resource "aws_cloudformation_stack" "sc_stack" {
   name          = "SCProductSetupStack"
   template_body = file("${path.module}/templates/sc_setup.yaml")
 
   parameters = {
     PortfolioId = aws_servicecatalog_portfolio.portfolio.id
-    ArtifactUrl = "https://${aws_s3_bucket.artifact_bucket.bucket}.s3.amazonaws.com/${aws_s3_object.helloworld_tar.key}"
+    ArtifactUrl = "https://${aws_s3_bucket.artifact_bucket.bucket}.s3.amazonaws.com/${aws_s3_object.helloworld_template.key}"
     RoleArn     = aws_iam_role.launch_role.arn
   }
 
   capabilities = ["CAPABILITY_NAMED_IAM"]
 }
 
-# Share the Portfolio across the Organization
+# Share the Portfolio with the organization
 resource "aws_servicecatalog_portfolio_share" "org_share" {
   portfolio_id = aws_servicecatalog_portfolio.portfolio.id
-  principal_id = var.account_id
+  principal_id = var.organization_id  # Must be o-xxxxxxxx
   type         = "ORGANIZATION"
 }
