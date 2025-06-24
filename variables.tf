9aa3111ca0a3732d6ac66fa5d41120fa431a9030
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-24 19:03:20 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,29 @@
-variable "tfe_token" {
-  description = "Terraform Cloud API token"
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "bucket_name" {
+  default = "sc-artifact-demo-helloworld" # Must be globally unique
+}
+
+variable "helloworld_source_path" {
+  description = "Path to the HelloWorld tar.gz downloaded from GitHub"
   type        = string
 }
 
-variable "tfc_organization" {
-  description = "Terraform Cloud organization name"
-  type        = string
+variable "portfolio_name" {
+  default = "HelloWorldPortfolio"
 }
 
-variable "aws_organization_id" {
-  description = "AWS Organization ID"
+variable "portfolio_owner" {
+  default = "BM"
+}
+
+variable "product_name" {
+  default = "HelloWorldProduct"
+}
+
+variable "organization_id" {
+  description = "Your AWS Organization ID (e.g., o-xxxxxxxxxx)"
   type        = string
 }
